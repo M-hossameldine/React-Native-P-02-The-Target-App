eae4cf8782ee85e2e14f28754a07276e56869598
M-hossameldine <m.hossameldine@gmail.com>
2025-07-15 21:32:08 +0300

    feature: add Title component and GameScreen title

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import Title from "../components/Title";
+
 export default function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
     </View>
   );
 }
@@ -11,6 +13,6 @@ export default function GameScreen() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 12,
+    padding: 24,
   },
 });
